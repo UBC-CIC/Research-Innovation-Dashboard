2c186e48cc4758e906ab7e496eb0a2d6587dce91
Christy Lam <christy.lam28@gmail.com>
2022-07-13 13:53:36 -0700

    Add PublicationGraph component

--- src/Rankings/Rankings.js
+++ src/Rankings/Rankings.js
@@ -1,146 +1,174 @@
-import * as React from 'react';
-import RankingsNavigation from "./Rankings_Navigation"
-import './Rankings.css'
-import { useState, useEffect } from 'react';
+import * as React from "react";
+import RankingsNavigation from "./Rankings_Navigation";
+import "./Rankings.css";
+import { useState, useEffect } from "react";
 
-import Amplify from '@aws-amplify/core'
-import { Auth } from '@aws-amplify/auth'
-import awsmobile from '../aws-exports'
+import Amplify from "@aws-amplify/core";
+import { Auth } from "@aws-amplify/auth";
+import awsmobile from "../aws-exports";
 
-import { API } from 'aws-amplify';
+import { API } from "aws-amplify";
 import {
-    getResearcherRankingsByDepartment,
-    getAllDepartments,
-    getAllResearchersRankings,
-    getAllFaculty,
-    getResearcherRankingsByFaculty,
-} from '../graphql/queries';
+  getResearcherRankingsByDepartment,
+  getAllDepartments,
+  getAllResearchersRankings,
+  getAllFaculty,
+  getResearcherRankingsByFaculty,
+} from "../graphql/queries";
 
-import RankingsByDepartment from './RankResearcherByDepartment'
-import AllResearcherRankings from './allResearcherRankings'
-import RankByFaculty from './RankByFaculty'
-import LoadingWheel from '../LoadingWheel'
+import RankingsByDepartment from "./RankResearcherByDepartment";
+import AllResearcherRankings from "./allResearcherRankings";
+import RankByFaculty from "./RankByFaculty";
+import LoadingWheel from "../LoadingWheel";
 
-Amplify.configure(awsmobile)
-Auth.configure(awsmobile)
+Amplify.configure(awsmobile);
+Auth.configure(awsmobile);
 
 export default function Rankings(props) {
-    const [researcherRankingsByDepartment, setResearcherRankingsByDepartment] = useState([]);
-    const [departmentToRank, setDepartmentToRank] = useState("");
-    const [allDepartments, setAllDepartments] = useState([]);
-    const [allResearcherRankings, setAllResearcherRankings] = useState([]);
+  const [
+    researcherRankingsByDepartment,
+    setResearcherRankingsByDepartment,
+  ] = useState([]);
+  const [departmentToRank, setDepartmentToRank] = useState("");
+  const [allDepartments, setAllDepartments] = useState([]);
+  const [allResearcherRankings, setAllResearcherRankings] = useState([]);
 
-    const [researcherRankingsByFaculty, setResearcherRankingsByFaculty] = useState([]);
-    const [facultyToRank, setFacultyToRank] = useState("");
-    const [allFaculty, setAllFaculty] = useState([]);
+  const [
+    researcherRankingsByFaculty,
+    setResearcherRankingsByFaculty,
+  ] = useState([]);
+  const [facultyToRank, setFacultyToRank] = useState("");
+  const [allFaculty, setAllFaculty] = useState([]);
 
-    const [showByDepartment, setShowByDepartment] = useState(true);
-    const [showByFaculty, setShowByFaculty] = useState(false);
-    const [showOverallRankings, setShowOverallRankings] = useState(false);
+  const [showByDepartment, setShowByDepartment] = useState(true);
+  const [showByFaculty, setShowByFaculty] = useState(false);
+  const [showOverallRankings, setShowOverallRankings] = useState(false);
 
-    const [pageLoaded, setPageLoaded] = useState(false);
+  const [pageLoaded, setPageLoaded] = useState(false);
 
-    const getDeparmentArray = async () => {
-        const department = await API.graphql({
-            query: getAllDepartments
-        });
-        let allDepartmentsArray = department.data.getAllDepartments;
-        setAllDepartments(allDepartmentsArray);
-        setDepartmentToRank(allDepartmentsArray[0]);
-    }
+  const getDeparmentArray = async () => {
+    const department = await API.graphql({
+      query: getAllDepartments,
+    });
+    let allDepartmentsArray = department.data.getAllDepartments;
+    setAllDepartments(allDepartmentsArray);
+    setDepartmentToRank(allDepartmentsArray[0]);
+  };
 
-    const getResearcherRankingByDepartment = async () => {
-        const rankings = await API.graphql({
-            query: getResearcherRankingsByDepartment,
-            variables: {prime_department: departmentToRank}, 
-        });
-        let researcher_rankings_by_department = rankings.data.getResearcherRankingsByDepartment;
-        setResearcherRankingsByDepartment(researcher_rankings_by_department);
-    }
+  const getResearcherRankingByDepartment = async () => {
+    const rankings = await API.graphql({
+      query: getResearcherRankingsByDepartment,
+      variables: { prime_department: departmentToRank },
+    });
+    let researcher_rankings_by_department =
+      rankings.data.getResearcherRankingsByDepartment;
+    setResearcherRankingsByDepartment(researcher_rankings_by_department);
+  };
 
-    const getFacultyArray = async () => {
-        const department = await API.graphql({
-            query: getAllFaculty
-        });
-        let allFacultyArray = department.data.getAllFaculty;
-        console.log("ALL FACULTY");
-        console.log(allFacultyArray);
-        setAllFaculty(allFacultyArray);
-        setFacultyToRank(allFacultyArray[0])
-    }
+  const getFacultyArray = async () => {
+    const department = await API.graphql({
+      query: getAllFaculty,
+    });
+    let allFacultyArray = department.data.getAllFaculty;
+    setAllFaculty(allFacultyArray);
+    setFacultyToRank(allFacultyArray[0]);
+  };
 
-    const getResearcherRankingByFaculty = async () => {
-        const rankings = await API.graphql({
-            query: getResearcherRankingsByFaculty,
-            variables: {prime_faculty: facultyToRank}, 
-        });
-        let researcher_rankings_by_Faculty = rankings.data.getResearcherRankingsByFaculty;
-        setResearcherRankingsByFaculty(researcher_rankings_by_Faculty);
-    }
+  const getResearcherRankingByFaculty = async () => {
+    const rankings = await API.graphql({
+      query: getResearcherRankingsByFaculty,
+      variables: { prime_faculty: facultyToRank },
+    });
+    let researcher_rankings_by_Faculty =
+      rankings.data.getResearcherRankingsByFaculty;
+    setResearcherRankingsByFaculty(researcher_rankings_by_Faculty);
+  };
 
-    const getOverallResearcherRankings = async () => {
-        const rankings = await API.graphql({
-            query: getAllResearchersRankings
-        });
+  const getOverallResearcherRankings = async () => {
+    const rankings = await API.graphql({
+      query: getAllResearchersRankings,
+    });
 
-        let allResearchRankingArray = rankings.data.getAllResearchersRankings;
-        setAllResearcherRankings(allResearchRankingArray);
-    }
+    let allResearchRankingArray = rankings.data.getAllResearchersRankings;
+    setAllResearcherRankings(allResearchRankingArray);
+  };
 
-    function changeDepartmentToRank(event) {
-        setDepartmentToRank(event.target.value);
-    }
+  function changeDepartmentToRank(event) {
+    setDepartmentToRank(event.target.value);
+  }
 
-    function changeFacultyToRank(event) {
-        setFacultyToRank(event.target.value);
-    }
+  function changeFacultyToRank(event) {
+    setFacultyToRank(event.target.value);
+  }
 
-    useEffect(() => {
-        Promise.all([getDeparmentArray(), getFacultyArray(), getOverallResearcherRankings()]).then(()=>{
-            setPageLoaded(true);
-        })
-    }, []);
+  useEffect(() => {
+    Promise.all([
+      getDeparmentArray(),
+      getFacultyArray(),
+      getOverallResearcherRankings(),
+    ]).then(() => {
+      setPageLoaded(true);
+    });
+  }, []);
 
-    useEffect(() => {
-        console.log("department to rank use effect called");
-        getResearcherRankingByDepartment();
-    }, [departmentToRank]);
+  useEffect(() => {
+    getResearcherRankingByDepartment();
+  }, [departmentToRank]);
 
-    useEffect(() => {
-        getResearcherRankingByFaculty();
-    }, [facultyToRank]);
+  useEffect(() => {
+    getResearcherRankingByFaculty();
+  }, [facultyToRank]);
 
-    function byDepartmentButton(){
-        setShowByDepartment(true);
-        setShowByFaculty(false);
-        setShowOverallRankings(false);
-    }
-    function byFacultyButton(){
-        setShowByDepartment(false);
-        setShowByFaculty(true);
-        setShowOverallRankings(false);
-    }
-    function overallRankingsButton(){
-        setShowByDepartment(false);
-        setShowByFaculty(false);
-        setShowOverallRankings(true);
-    }
+  function byDepartmentButton() {
+    setShowByDepartment(true);
+    setShowByFaculty(false);
+    setShowOverallRankings(false);
+  }
+  function byFacultyButton() {
+    setShowByDepartment(false);
+    setShowByFaculty(true);
+    setShowOverallRankings(false);
+  }
+  function overallRankingsButton() {
+    setShowByDepartment(false);
+    setShowByFaculty(false);
+    setShowOverallRankings(true);
+  }
 
-
-    return(
+  return (
+    <div>
+      {!pageLoaded && <LoadingWheel />}
+      {pageLoaded && (
         <div>
-            {!pageLoaded && <LoadingWheel />}
-            {pageLoaded && <div>
-            <RankingsNavigation onClickFunctions={{byDepartmentButton, byFacultyButton, overallRankingsButton}} />
-            {showByDepartment && <RankingsByDepartment allDepartments={allDepartments} 
-            researcherRankingsByDepartment={researcherRankingsByDepartment}
-            changeDepartmentToRank={changeDepartmentToRank}
-            departmentToRank={departmentToRank} />}
-            {showByFaculty && <RankByFaculty allFaculty={allFaculty}
-            researcherRankingsByFaculty={researcherRankingsByFaculty}
-            changeFacultyToRank={changeFacultyToRank}/>}
-            {showOverallRankings && <AllResearcherRankings allResearcherRankings={allResearcherRankings} />}
-            </div>}
+          <RankingsNavigation
+            onClickFunctions={{
+              byDepartmentButton,
+              byFacultyButton,
+              overallRankingsButton,
+            }}
+          />
+          {showByDepartment && (
+            <RankingsByDepartment
+              allDepartments={allDepartments}
+              researcherRankingsByDepartment={researcherRankingsByDepartment}
+              changeDepartmentToRank={changeDepartmentToRank}
+              departmentToRank={departmentToRank}
+            />
+          )}
+          {showByFaculty && (
+            <RankByFaculty
+              allFaculty={allFaculty}
+              researcherRankingsByFaculty={researcherRankingsByFaculty}
+              changeFacultyToRank={changeFacultyToRank}
+            />
+          )}
+          {showOverallRankings && (
+            <AllResearcherRankings
+              allResearcherRankings={allResearcherRankings}
+            />
+          )}
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- src/UBC_Metrics/UbcMetrics.js
+++ src/UBC_Metrics/UbcMetrics.js
@@ -1,103 +1,105 @@
 import * as React from "react";
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
-import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import { useState, useEffect } from 'react';
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {useParams} from "react-router-dom";
-import ReactWordcloud from 'react-wordcloud';
+import { useParams } from "react-router-dom";
+import ReactWordcloud from "react-wordcloud";
 import DoughnutChart from "./DoughnutChart";
-import BarGraph from "./BarGraph";
+import { Grid, Box } from "@mui/material";
+import { API } from "aws-amplify";
 
-import { API } from 'aws-amplify';
-
-import {
-    wordCloud,
-    facultyMetrics,
-} from '../graphql/queries';
-
-
+import { wordCloud, facultyMetrics } from "../graphql/queries";
+import PublicationGraph from "./PublicationGraph";
 
 const options = {
-    colors: ["#23D2DC", "#2376DC", "#23DC89"],
-    enableTooltip: false,
-    deterministic: true,
-    fontFamily: "Arial",
-    fontSizes: [5, 60],
-    fontStyle: "normal",
-    fontWeight: "normal",
-    padding: 1,
-    rotations: 3,
-    rotationAngles: [0, 0],
-    scale: "sqrt",
-    spiral: "archimedean"
+  colors: ["#23D2DC", "#2376DC", "#23DC89"],
+  enableTooltip: false,
+  deterministic: true,
+  fontFamily: "Arial",
+  fontSizes: [5, 60],
+  fontStyle: "normal",
+  fontWeight: "normal",
+  padding: 1,
+  rotations: 3,
+  rotationAngles: [0, 0],
+  scale: "sqrt",
+  spiral: "archimedean",
 };
 
-export default function UbcMetrics(props){
-    const [words, setWords] = useState([]);
-    const [facultyList, setFacultyList] = useState([]);
-    const [facultyPublicationsOverall, setFacultyPublicationsOverall] = useState([]);
-    const [facultyPubsLastFiveYears, setFacultyPubsLastFiveYears] = useState([]);
-    const [facultyPubsLastTenYears, setFacultyPubsLastTenYears] = useState([]);
+export default function UbcMetrics(props) {
+  const [words, setWords] = useState([]);
+  const [facultyList, setFacultyList] = useState([]);
+  const [facultyPublicationsOverall, setFacultyPublicationsOverall] = useState(
+    []
+  );
+  const [facultyPubsLastFiveYears, setFacultyPubsLastFiveYears] = useState([]);
+  const [facultyPubsLastTenYears, setFacultyPubsLastTenYears] = useState([]);
 
+  const wordCloudQuery = async () => {
+    const wordCloudResult = await API.graphql({
+      query: wordCloud,
+    });
+    setWords(wordCloudResult.data.wordCloud);
+  };
+  const facultyMetricsQuery = async () => {
+    const facultyMetricsResult = await API.graphql({
+      query: facultyMetrics,
+    });
 
-    const wordCloudQuery = async () => {
-        const wordCloudResult = await API.graphql({
-            query: wordCloud
-        });
-        setWords(wordCloudResult.data.wordCloud);
+    let facList = [];
+    let facOverallPubs = [];
+    let fac5year = [];
+    let fac10year = [];
+
+    let result = facultyMetricsResult.data.facultyMetrics;
+
+    for (let i = 0; i < result.length; i++) {
+      facList.push(result[i].faculty);
+      facOverallPubs.push(result[i].num_publications);
+      fac5year.push(result[i].num_pubs_last_five_years);
+      fac10year.push(result[i].num_pubs_last_ten_years);
     }
-    const facultyMetricsQuery = async () => {
-        const facultyMetricsResult = await API.graphql({
-            query: facultyMetrics
-        });
+    setFacultyList(facList);
+    setFacultyPublicationsOverall(facOverallPubs);
+    setFacultyPubsLastFiveYears(fac5year);
+    setFacultyPubsLastTenYears(fac10year);
+  };
 
-        let facList = [];
-        let facOverallPubs = [];
-        let fac5year = [];
-        let fac10year = [];
+  useEffect(() => {
+    wordCloudQuery();
+    facultyMetricsQuery();
+  }, []);
 
-        let result = facultyMetricsResult.data.facultyMetrics;
+  let dataset = [
+    {
+      label: "5 Years",
+      data: facultyPubsLastFiveYears,
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "10 Years",
+      data: facultyPubsLastTenYears,
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ];
 
-        for(let i = 0; i<result.length; i++){
-            facList.push(result[i].faculty);
-            facOverallPubs.push(result[i].num_publications);
-            fac5year.push(result[i].num_pubs_last_five_years);
-            fac10year.push(result[i].num_pubs_last_ten_years);
-        }
-        setFacultyList(facList);
-        setFacultyPublicationsOverall(facOverallPubs);
-        setFacultyPubsLastFiveYears(fac5year);
-        setFacultyPubsLastTenYears(fac10year);
-    }
-
-    useEffect(() => {
-        wordCloudQuery();
-        facultyMetricsQuery();
-    }, []);
-
-    let dataset= [
-        {
-            label: '5 Years',
-            data: facultyPubsLastFiveYears,
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-        {
-            label: '10 Years',
-            data: facultyPubsLastTenYears,
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-    ]
-
-    console.log(dataset);
-
-    return(
-        <div style={{ height: 500, width: "100%" }}>
-            <ReactWordcloud options={options} words={words} />
-            {/* <DoughnutChart labels={facultyList} data={facultyPublicationsOverall} title={"Pubs Graph"}/> */}
-            {/* <BarGraph labels={facultyList} dataset={dataset} /> */}
-        </div>
-    );
+  return (
+    <Grid>
+      <ReactWordcloud options={options} words={words} />
+      {/* <DoughnutChart
+        labels={facultyList}
+        data={facultyPublicationsOverall}
+        title={"Pubs Graph"}
+      /> */}
+      <Grid item>
+        <Box sx={{ width: "100%", height: "300px" }}>
+          <PublicationGraph />
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
