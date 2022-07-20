4202a6f6929e2b5b2dbf6d9a0fc574c75af4e932
Christy Lam <christy.lam28@gmail.com>
2022-07-20 09:42:47 -0700

    Remove unnecessary imports

--- src/Search_Reseachers/AdvancedSearchRow.js
+++ src/Search_Reseachers/AdvancedSearchRow.js
@@ -1,31 +1,54 @@
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import React from 'react';
-import Stack from '@mui/material/Stack';
-import { useState } from 'react';
-import InputBase from '@mui/material/InputBase';
+import React from "react";
+import Stack from "@mui/material/Stack";
+import InputBase from "@mui/material/InputBase";
 
-export default function AdvancedSearchRow(props){
-    return(
-            <Grid container>
-            <Paper square={true} elevation={0} sx={{width: "100%", m: "2%"}} component={Stack} direction="row">
-                <Paper elevation={0} sx={{width: "20%", paddingRight: "2%"}} component={Stack} direction="column" justifyContent="center">
-                    <Typography variant='h7'>
-                        {props.whatDoesSearchBoxDo}
-                    </Typography>
-                </Paper>
-                <Paper square={true} elevation={0} sx={{width: "50%", border: 1}}>
-                    <InputBase onKeyDown={(e) => {if(e.key === 'Enter'){props.routeChange()}}} 
-                    value={props.searchBarValue} onChange={(e) => {props.setSearchBarValue(e.target.value)}} 
-                    fullWidth={true} sx={{padding: "8px", fontSize: "1.0rem"}}></InputBase>
-                </Paper>
-                <Paper elevation={0} sx={{width: "30%", paddingLeft: "2%"}} component={Stack} direction="column" justifyContent="center">
-                    <Typography variant='h7'>
-                        {props.howToDoSearchBox}
-                    </Typography>
-                </Paper>
-            </Paper>
-            </Grid>
-    );
+export default function AdvancedSearchRow(props) {
+  return (
+    <Grid container>
+      <Paper
+        square={true}
+        elevation={0}
+        sx={{ width: "100%", m: "2%" }}
+        component={Stack}
+        direction="row"
+      >
+        <Paper
+          elevation={0}
+          sx={{ width: "20%", paddingRight: "2%" }}
+          component={Stack}
+          direction="column"
+          justifyContent="center"
+        >
+          <Typography variant="h7">{props.whatDoesSearchBoxDo}</Typography>
+        </Paper>
+        <Paper square={true} elevation={0} sx={{ width: "50%", border: 1 }}>
+          <InputBase
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                props.routeChange();
+              }
+            }}
+            value={props.searchBarValue}
+            onChange={(e) => {
+              props.setSearchBarValue(e.target.value);
+            }}
+            fullWidth={true}
+            sx={{ padding: "8px", fontSize: "1.0rem" }}
+          ></InputBase>
+        </Paper>
+        <Paper
+          elevation={0}
+          sx={{ width: "30%", paddingLeft: "2%" }}
+          component={Stack}
+          direction="column"
+          justifyContent="center"
+        >
+          <Typography variant="h7">{props.howToDoSearchBox}</Typography>
+        </Paper>
+      </Paper>
+    </Grid>
+  );
 }
