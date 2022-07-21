50839b02d90a01269a9d296378bbd9e7348033d9
stefansson30952 <stefansson30952@gmail.com>
2022-07-21 15:16:09 -0700

    Documented CDK stacks

--- updatePublicationsDocker/cdk/lib/opensearch-stack.ts
+++ updatePublicationsDocker/cdk/lib/opensearch-stack.ts
@@ -14,14 +14,13 @@ import * as cdk from 'aws-cdk-lib'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { ArnPrincipal, Effect, PolicyDocument, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
 export class OpensearchStack extends Stack {
     public readonly devDomain: opensearch.Domain;
 
     constructor(scope: Construct, id: string, vpcStack: VpcStack, props?: StackProps) {
     super(scope, id, props);
 
+    //Create a role for lambda to access opensearch
     const lambdaRole = new Role(this, 'OpenSearchLambdaRole', {
         roleName: 'OpenSearchLambdaRole',
         assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
@@ -45,7 +44,7 @@ export class OpensearchStack extends Stack {
         },
     });
     
-    //Allow 
+    //Create a policy statemnet to allow the lambda to access opensearch 
     const openSearchPolicyStatement = new PolicyStatement({
         effect: iam.Effect.ALLOW,
         actions: [ 'es:*' ],
@@ -53,12 +52,15 @@ export class OpensearchStack extends Stack {
         resources: [ '*' ],
     });
 
+    // create opensearch service linked role. Without this role you cannot attach a vpc to opensearch
     const openSearchVPCPermissions = new iam.CfnServiceLinkedRole(this, 'OpenSearchSLR', {
         awsServiceName: 'opensearchservice.amazonaws.com'
     });
 
+    // get the default security group from the vpc
     const defaultSecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, id, vpcStack.vpc.vpcDefaultSecurityGroup);
     
+    // Create the opensearch domain
     this.devDomain = new opensearch.Domain(this, 'vpriCdkOpensearchDomain', {
         version: opensearch.EngineVersion.OPENSEARCH_1_1,
         enableVersionUpgrade: true,
@@ -77,6 +79,7 @@ export class OpensearchStack extends Stack {
     //Attach vpc service linked role to opensearch domain
     this.devDomain.node.addDependency(openSearchVPCPermissions);
 
+    // Create three layers for the opensearch query function
     const aws4Layer = new lambda.LayerVersion(this, 'aws4', {
         compatibleRuntimes: [
           lambda.Runtime.NODEJS_12_X,
@@ -107,6 +110,7 @@ export class OpensearchStack extends Stack {
         description: 'opensearch',
     });
 
+    // Create the openserach query function.
     const fn = new lambda.Function(this, 'MyFunction', {
         functionName: "QueryOpensearch",
         runtime: lambda.Runtime.NODEJS_16_X,
