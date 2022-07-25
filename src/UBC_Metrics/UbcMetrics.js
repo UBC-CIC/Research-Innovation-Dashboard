bc051358ad64efa05e4bb8da30567aace1777494
Christy Lam <christy.lam28@gmail.com>
2022-07-25 13:42:07 -0700

    Add date range slider to WordCloud, update styling, refactor queries in UbcMetrics

--- src/UBC_Metrics/UbcMetrics.js
+++ src/UBC_Metrics/UbcMetrics.js
@@ -1,146 +1,50 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import ReactWordcloud from "react-wordcloud";
+import React, { useState, useEffect } from "react";
 import DoughnutChart from "./DoughnutChart";
-import { select } from "d3-selection";
-import { Grid, Box, Typography } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import { API } from "aws-amplify";
-import {
-  wordCloud,
-  facultyMetrics,
-  allPublicationsPerFacultyQuery,
-} from "../graphql/queries";
+import { allPublicationsPerFacultyQuery } from "../graphql/queries";
 import PublicationGraph from "./PublicationGraph";
+import WordCloud from "./WordCloud";
 
-import "tippy.js/dist/tippy.css";
-import "tippy.js/animations/scale.css";
-
-function getCallback(callback) {
-  return function (word, event) {
-    const isActive = callback !== "onWordMouseOut";
-    const element = event.target;
-    const text = select(element);
-    text.on("click", () => {
-      if (isActive) {
-        window.open(`${window.location.origin}/${word.text}`);
-      }
-    });
-    // .transition()
-    // .attr("background", "white")
-    // .attr("font-size", isActive ? "300%" : "100%")
-    // .attr("text-decoration", isActive ? "underline" : "none");
-  };
-}
-
-const options = {
-  colors: ["#23D2DC", "#2376DC", "#23DC89"],
-  enableTooltip: true,
-  deterministic: true,
-  fontFamily: "Arial",
-  fontSizes: [5, 60],
-  fontStyle: "normal",
-  fontWeight: "normal",
-  padding: 1,
-  rotations: 3,
-  rotationAngles: [0, 0],
-  scale: "sqrt",
-  spiral: "archimedean",
-};
-const callbacks = {
-  getWordTooltip: (word) =>
-    `The word "${word.text}" appears ${word.value} times. Click to search for the keyword "${word.text}"`,
-  onWordClick: getCallback("onWordClick"),
-  onWordMouseOut: getCallback("onWordMouseOut"),
-  onWordMouseOver: getCallback("onWordMouseOver"),
-};
-
-export default function UbcMetrics(props) {
-  const [words, setWords] = useState([]);
+export default function UbcMetrics() {
   const [facultyList, setFacultyList] = useState([]);
   const [facultyPublicationsOverall, setFacultyPublicationsOverall] = useState(
     []
   );
-
-  const wordCloudQuery = async () => {
-    const wordCloudResult = await API.graphql({
-      query: wordCloud,
-      variables: { gte: 1908, lte: 2022 },
-    });
-    setWords(wordCloudResult.data.wordCloud);
-  };
-  const facultyMetricsQuery = async () => {
-    const facultyMetricsResult = await API.graphql({
-      query: facultyMetrics,
-    });
-
-    let facList = [];
-    let facOverallPubs = [];
-    let fac5year = [];
-    let fac10year = [];
-
-    let result = facultyMetricsResult.data.facultyMetrics;
-
-    for (let i = 0; i < result.length; i++) {
-      facList.push(result[i].faculty);
-      facOverallPubs.push(result[i].num_publications);
-      fac5year.push(result[i].num_pubs_last_five_years);
-      fac10year.push(result[i].num_pubs_last_ten_years);
-    }
-    setFacultyList(facList);
-    setFacultyPublicationsOverall(facOverallPubs);
-  };
 
   const allPublicationsPerFacultyFunction = async () => {
     const queryResult = await API.graphql({
       query: allPublicationsPerFacultyQuery,
     });
 
-    let facList = [];
-    let facOverallPubs = [];
-
-    let result = queryResult.data.allPublicationsPerFacultyQuery;
-
-    for (let i = 0; i < result.length; i++) {
-      facList.push(result[i].faculty);
-      facOverallPubs.push(result[i].sum);
-    }
+    const queryResultData = queryResult.data.allPublicationsPerFacultyQuery;
+    const facList = queryResultData.map(
+      (facultyResult) => facultyResult.faculty
+    );
+    const facOverallPubs = queryResultData.map(
+      (facultyResult) => facultyResult.sum
+    );
     setFacultyList(facList);
     setFacultyPublicationsOverall(facOverallPubs);
   };
 
   useEffect(() => {
-    wordCloudQuery();
-    facultyMetricsQuery();
     allPublicationsPerFacultyFunction();
   }, []);
 
   return (
-    <Grid container>
-      <Grid item xs={12} sx={{ mt: "1%" }}>
-        <Typography variant="h4" align="center">
-          Top 100 Keywords In UBC Research
-        </Typography>
+    <Grid container sx={{ px: "1em" }} columnSpacing={4}>
+      <Grid item xs={12} sx={{ py: "4em" }}>
+        <WordCloud />
       </Grid>
-      <Grid
-        item
-        xs={12}
-        sx={{
-          height: "70vh",
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "row",
-        }}
-      >
-        <ReactWordcloud callbacks={callbacks} options={options} words={words} />
-      </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={5}>
         <DoughnutChart
           labels={facultyList}
           data={facultyPublicationsOverall}
           title={"Pubs Graph"}
         />
       </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={7}>
         <Box sx={{ width: "100%", height: "369px" }}>
           <PublicationGraph />
         </Box>
---
+++ src/UBC_Metrics/WordCloud.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from "react";
+import ReactWordcloud from "react-wordcloud";
+import { select } from "d3-selection";
+import { Typography, Slider, Box, CircularProgress } from "@mui/material";
+import { API } from "aws-amplify";
+import { wordCloud } from "../graphql/queries";
+
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+
+function getCallback(callback) {
+  return function (word, event) {
+    const isActive = callback !== "onWordMouseOut";
+    const element = event.target;
+    const text = select(element);
+    text.on("click", () => {
+      if (isActive) {
+        window.open(`${window.location.origin}/${word.text}`);
+      }
+    });
+  };
+}
+
+const WordCloud = () => {
+  const [words, setWords] = useState([]);
+  const currentYear = new Date().getFullYear();
+  const [dateRange, setDateRange] = useState([1908, currentYear]);
+  const options = {
+    colors: ["#23D2DC", "#2376DC", "#23DC89"],
+    enableTooltip: true,
+    deterministic: true,
+    fontFamily: "Arial",
+    fontSizes: [5, 60],
+    fontStyle: "normal",
+    fontWeight: "normal",
+    padding: 1,
+    rotations: 3,
+    rotationAngles: [0, 0],
+    scale: "sqrt",
+    spiral: "archimedean",
+  };
+
+  const callbacks = {
+    getWordTooltip: (word) =>
+      `The word "${word.text}" appears ${word.value} times. Click to search for the keyword "${word.text}"`,
+    onWordClick: getCallback("onWordClick"),
+    onWordMouseOut: getCallback("onWordMouseOut"),
+    onWordMouseOver: getCallback("onWordMouseOver"),
+  };
+
+  const wordCloudQuery = async () => {
+    const wordCloudResult = await API.graphql({
+      query: wordCloud,
+      variables: { gte: dateRange[0], lte: dateRange[1] },
+    });
+    setWords(wordCloudResult.data.wordCloud);
+  };
+
+  useEffect(() => {
+    dateRange && wordCloudQuery();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dateRange]);
+
+  const handleChange = (event, newValue) => {
+    setDateRange(newValue);
+  };
+
+  const valueText = (value) => {
+    return `${value}°C`;
+  };
+
+  return (
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Typography variant="h4" align="center">
+        Top 100 Keywords In UBC Research
+      </Typography>
+      {words.length === 0 ? (
+        <CircularProgress sx={{ py: "5em" }} />
+      ) : (
+        dateRange && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              pt: "2em",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Box
+              sx={{
+                width: "30%",
+                m: "1.5em 0 2em 0",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: { xs: "column", md: "row" },
+                  justifyContent: "center",
+                }}
+              >
+                <Typography>Date Range:&nbsp;</Typography>
+                <Typography sx={{ color: "#0055B7", fontWeight: 700 }}>
+                  {dateRange[0]} - {dateRange[1]}
+                </Typography>
+              </Box>
+              <Slider
+                getAriaLabel={() => "Date range"}
+                value={dateRange}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                getAriaValueText={valueText}
+                min={1908}
+                max={currentYear}
+              />
+            </Box>
+            <ReactWordcloud
+              callbacks={callbacks}
+              options={options}
+              words={words}
+            />
+          </Box>
+        )
+      )}
+    </Box>
+  );
+};
+
+export default WordCloud;
