9c552f3ae7be328858546bc6ce41e95f7656aedc
Lance Holland <lanceholland01@gmail.com>
2022-08-31 10:01:51 -0700

    added missing column to createTables

--- back_end/cdk/lambda/createTables/createTables.py
+++ back_end/cdk/lambda/createTables/createTables.py
@@ -49,6 +49,7 @@ def lambda_handler(event, context):
     columns.append(createColumn('campus', 'character varying', '', False))
     columns.append(createColumn('scopus_id', 'character varying', '', False))
     columns.append(createColumn('extra_ids', 'character varying[]', '', False))
+    columns.append(createColumn('pub_ids', 'character varying[]', '', False))
     columns.append(createColumn('keywords', 'character varying', '(1000000)', False))
     columns.append(createColumn('last_updated', 'character varying', '', True))
     query = createQuery('researcher_data', columns)
